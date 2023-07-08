7ff189e9c707982f3b046ba9802834f19d25db21
benjazzy <benjazzy64@gmail.com>
2023-07-07 23:57:25 -0400

    Finished queued_sender tests.

--- queued_sender/src/error.rs
+++ queued_sender/src/error.rs
@@ -1,24 +1,19 @@
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq)]
 pub struct SendError<T>(pub T);
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq, Eq)]
 pub enum ReadyError<T> {
     #[error("there was a problem sending the message")]
     Send(SendError<T>),
 
     #[error("the sender was already ready")]
     AlreadyReady,
-
-    // #[error("the sender is still waiting for an ok from the last message")]
-    // Waiting,
 }
 
-#[derive(Debug, Error)]
-pub enum OkError<T> {
-    #[error("there was a problem sending the message")]
-    Send(SendError<T>),
-    
-    
-}
+// #[derive(Debug, Error, PartialEq, Eq)]
+// pub enum OkError<T> {
+//     #[error("there was a problem sending the message")]
+//     Send(SendError<T>),
+// }
--- queued_sender/src/lib.rs
+++ queued_sender/src/lib.rs
@@ -1,15 +1,13 @@
 pub mod error;
 
-use std::collections::VecDeque;
-use futures_util::SinkExt;
-use thiserror::Error;
 use error::{ReadyError, SendError};
+use futures_util::SinkExt;
+use std::collections::VecDeque;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum SenderState {
     Ready,
     Sending,
-    // Waiting,
 }
 
 #[derive(Debug)]
@@ -40,13 +38,17 @@ where
 }
 
 impl<S, M> QueuedSender<S, M>
-    where
-        S: SinkExt<M> + Unpin,
+where
+    S: SinkExt<M> + Unpin,
 {
     pub async fn send(&mut self, item: M) -> Result<(), SendError<S::Error>> {
         self.queue.push_back(item);
 
-        self.update_queue().await
+        if let SenderState::Ready = self.state {
+            self.update_queue().await
+        } else {
+            Ok(())
+        }
     }
 
     pub async fn ready(&mut self) -> Result<(), ReadyError<S::Error>> {
@@ -64,6 +66,7 @@ impl<S, M> QueuedSender<S, M>
     async fn update_queue(&mut self) -> Result<(), SendError<S::Error>> {
         if let SenderState::Ready = self.state {
             if let Some(message) = self.queue.pop_front() {
+                self.state = SenderState::Sending;
                 return self.sender.send(message).await.map_err(SendError);
             }
         }
@@ -74,52 +77,127 @@ impl<S, M> QueuedSender<S, M>
 
 #[cfg(test)]
 mod tests {
-    use std::pin::Pin;
-    use std::sync::Arc;
-    use std::task::{Context, Poll};
-    use tokio::sync::Mutex;
-    use futures_util::Sink;
     use super::*;
 
-    enum MockSinkInner {
-        Ready
-    }
-    struct MockSink<'a, T>(&'a mut Vec<T>);
+    #[tokio::test]
+    async fn check_start_ready() {
+        let mut out: Vec<&'static str> = vec![];
 
-    impl<'a, T> Sink<T> for MockSink<'a, T> {
-        type Error = ();
-
-        fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-            Poll::Ready(Ok(()))
-        }
-
-        fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
-            (*self).0.push(item);
-
-            Ok(())
-        }
-
-        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-            Poll::Ready(Ok(()))
-        }
-
-        fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-            Poll::Ready(Ok(()))
-        }
+        let sender = QueuedSender::new(&mut out);
+        assert_eq!(sender.state, SenderState::Ready);
     }
 
     #[tokio::test]
-    async fn check_send_and_ready() {
+    async fn check_send() {
         let message = "test_message";
         let mut out: Vec<&'static str> = vec![];
 
-        let mut sender = QueuedSender::new(MockSink(&mut out));
-        assert_eq!(sender.state, SenderState::Ready);
-
+        let mut sender = QueuedSender::new(&mut out);
         sender.send(message).await.expect("Problem sending message");
         assert_eq!(sender.state, SenderState::Sending);
 
-        assert_eq!(out.len(), 1);
+        assert_eq!(out, vec![message]);
+    }
 
+    #[tokio::test]
+    async fn check_dont_send_when_sending() {
+        let message = "test_message";
+        let mut out: Vec<&'static str> = vec![];
+
+        let mut sender = QueuedSender::new(&mut out);
+        sender.state = SenderState::Sending;
+        sender.send(message).await.expect("Problem sending message");
+
+        assert_eq!(sender.state, SenderState::Sending);
+        assert_eq!(sender.queue.get(0), Some(&message));
+
+        assert!(out.is_empty());
+    }
+
+    #[tokio::test]
+    async fn check_ready() {
+        let mut out: Vec<&'static str> = vec![];
+
+        let mut sender = QueuedSender::new(&mut out);
+        sender.state = SenderState::Sending;
+
+        sender
+            .ready()
+            .await
+            .expect("Problem setting sender to ready");
+
+        assert_eq!(sender.state, SenderState::Ready);
+
+        assert!(out.is_empty());
+    }
+
+    #[tokio::test]
+    async fn check_double_ready() {
+        let mut out: Vec<&'static str> = vec![];
+
+        let mut sender = QueuedSender::new(&mut out);
+        assert_eq!(sender.state, SenderState::Ready);
+
+        assert_eq!(
+            sender
+                .ready()
+                .await
+                .expect_err("Sender didn't return an error for double ready"),
+            ReadyError::AlreadyReady,
+        );
+
+        assert_eq!(sender.state, SenderState::Ready);
+
+        assert!(out.is_empty());
+    }
+
+    #[tokio::test]
+    async fn check_send_on_ready() {
+        let message = "test_message";
+        let mut out: Vec<&'static str> = vec![];
+
+        let mut sender = QueuedSender::new(&mut out);
+        sender.state = SenderState::Sending;
+
+        sender.send(message).await.expect("Problem sending message");
+        assert_eq!(sender.state, SenderState::Sending);
+        assert_eq!(sender.queue.len(), 1);
+
+        sender
+            .ready()
+            .await
+            .expect("Problem setting sender to ready");
+        assert_eq!(sender.state, SenderState::Sending);
+        assert!(sender.queue.is_empty());
+
+        assert_eq!(out, vec![message]);
+    }
+
+    #[tokio::test]
+    async fn check_order() {
+        let num_messages = 2;
+        assert!(num_messages > 1);
+
+        let mut out: Vec<i32> = vec![];
+
+        let mut sender = QueuedSender::new(&mut out);
+        sender.state = SenderState::Sending;
+
+        for i in 0..num_messages {
+            sender.send(i).await.expect("Problem sending message")
+        }
+
+        assert_eq!(sender.queue.len(), num_messages as usize);
+
+        for _ in 0..num_messages {
+            sender
+                .ready()
+                .await
+                .expect("Problem setting sender to ready");
+        }
+
+        for i in 0..num_messages {
+            assert_eq!(out.get(i as usize), Some(&i));
+        }
     }
 }
